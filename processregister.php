468435f8d5979f2478e0fc6f6ae135d36f16df6d
Oluwanifemi Bamgbose <oluwanifemibamgbose@Oluwanifemis-MacBook-Pro.local>
2020-04-01 21:37:28 +0100

    Added validation in php

--- processregister.php
+++ processregister.php
@@ -1,24 +1,43 @@
 <?php 
 
-print_r($_POST);
+session_start();
 
-
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$gender = $_POST['gender'];
-$designation = $_POST['designation'];
-$department = $_POST['department'];
-
-$errorArray = [];
-
+$errorCount = 0;
 
 //Verification and Validation
+$first_name = $_POST['first_name'] != "" ? $_POST['first_name'] : $errorCount++;
+$last_name = $_POST['last_name'] != "" ? $_POST['last_name'] : $errorCount++;
+$email = $_POST['email'] != "" ? $_POST['email'] : $errorCount++;
 
-if($first_name == ""){
-    $errorArray = "first name cannot be empty";
+$password = $_POST['password'] != "" ? $_POST['password'] : $errorCount++;
+$gender = $_POST['gender'] != "" ? $_POST['gender'] : $errorCount++;
+$designation = $_POST['designation'] != "" ? $_POST['designation'] : $errorCount++;
+$department = $_POST['department'] != "" ? $_POST['department'] : $errorCount++;
+
+
+$_SESSION['first_name']= $first_name;
+$_SESSION['last_name']= $last_name;
+$_SESSION['email']= $email;
+$_SESSION['gender']= $gender;
+$_SESSION['designation']= $designation;
+$_SESSION['department']= $department;
+
+if($errorCount > 0){
+    //redirect back and display error
+    $_SESSION['error'] = 'You have '. $errorCount .' errors In your form submission';
+   header("Location: register.php");
+}else{
+    //continue
+    echo "Success";
 }
 
-print_r($errorArray);
+
+
+//saving the data into the db (folder)
+
+
+
+//
+
+
 ?>
--- register.php
+++ register.php
@@ -1,35 +1,67 @@
-<?php include_once('lib/header.php');   ?> 
+<?php session_start(); include_once('lib/header.php');   ?> 
     <p><strong> Welcome, Please Register</strong></p>
     <p>All Fields are required</p>
 
     <form action="processregister.php" method="post">
+    <p>
+    <?php 
+        if(isset($_SESSION['error'])&& !empty($_SESSION['error'])){
+            echo $_SESSION['error'];
+            session_destroy();
+        }
+    ?>
+    </p>
         <p>
             <label for="first_name">First Name</label>
-            <input type="text" name="first_name" placeholder= "First Name" required >
+            <input 
+                <?php 
+                    if(isset($_SESSION['first_name'])){
+                        echo "value=" .$_SESSION['first_name'];
+                    }
+                ?>
+            type="text" name="first_name" placeholder= "First Name" >
         </p>
         <p>
             <label for="last_name">Last Name</label>
-            <input type="text" name="last_name" placeholder= "Last Name" required>
+            <input 
+            <?php 
+                    if(isset($_SESSION['last_name'])){
+                        echo "value=" .$_SESSION['last_name'];
+                    }
+                ?>
+            type="text" name="last_name" placeholder= "Last Name" >
         </p>
         <p>
             <label for="email">Email</label>
-            <input type="email" name="email" placeholder= "Email" required>
+            <input
+            <?php 
+                    if(isset($_SESSION['email'])){
+                        echo "value=" .$_SESSION['email'];
+                    }
+                ?>
+            type="email" name="email" placeholder= "Email" >
         </p>
         <p>
-            <label for="gender">Gender</label required>
+            <label for="gender">Gender</label >
             <select name="gender" >
                 <option value="">Select One</option>
-                <option >Male</option>
+                <option 
+                <?php 
+                    if(isset($_SESSION['gender'])&& $_SESSION['gender']=='Male'){
+                        echo "selected";
+                    }
+                ?>
+                >Male</option>
                 <option >Female</option>
             </select>
         </p>
         <p>
             <label for="password">Password</label>
-            <input type="password" name="password" placeholder= "Password" required>
+            <input type="password" name="password" placeholder= "Password" >
         </p>
 
         <p>
-            <label for="designation">Designation</label required>
+            <label for="designation">Designation</label >
             <select name="designation" >
                 <option value="">Select One</option>
                 <option >Medical Team(MT)</option>
@@ -38,7 +70,13 @@
         </p>
         <p>
             <label for="department">Department</label>
-            <input type="text" name="department" placeholder="Department" required>
+            <input 
+            <?php 
+                    if(isset($_SESSION['department'])){
+                        echo "value=" .$_SESSION['department'];
+                    }
+                ?>
+            type="text" name="department" placeholder="Department" >
         </p>
         <p>
         <button type="submit">Register</button>

4165d6550fdfe81d0f18cb6d10a006cdcba9f312
Oluwanifemi Bamgbose <oluwanifemibamgbose@Oluwanifemis-MacBook-Pro.local>
2020-04-02 13:15:08 +0100

    Re add processregister

--- login.php
+++ login.php
@@ -1,3 +1,14 @@
-<?php include_once('lib/header.php'); ?> 
+<?php include_once('lib/header.php'); session_start(); ?> 
 Login Form here 
+
+<p>
+    <?php 
+        if(isset($_SESSION['message'])&& !empty($_SESSION['message'])){
+            echo "<span style='color:green'>".$_SESSION['message']. "<span/>";
+            session_destroy();
+        }
+    ?>
+</p>
+
+
 <?php include_once('lib/footer.php'); ?>
